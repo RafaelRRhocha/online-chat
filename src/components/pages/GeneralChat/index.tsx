3d6b10bd0d80027bddb5405949e380c640ab9a9f
Rafael <1rafaelrocha2@gmail.com>
2022-09-22 18:17:28 -0300

    feat: created new chat

--- src/components/pages/GeneralChat/index.tsx
+++ src/components/pages/GeneralChat/index.tsx
@@ -1,14 +1,33 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import ChatRoom from '../../templates/ChatRoom';
 import * as styles from './styles';
 
 interface GeneralChatProps {}
 
 const GeneralChat: FC<GeneralChatProps> = ({}) => {
+  const [messages, setMessages] = useState('');
+
   return (
     <div className={ styles.chatDiv }>
       <ChatRoom />
-      <input type="text" placeholder="Digite Aqui" className={ styles.chatInput } />
+      <div className={ styles.userInputs }>
+        <input
+          type="text"
+          value={ messages }
+          name="messages"
+          onChange={({ target: { value }}) => setMessages(value)}
+          placeholder="Digite Aqui"
+          className={ styles.chatInput }
+        />
+        <button
+          type="button"
+          className={ styles.btn }
+          onClick={() => alert('Em Desenvolvimento!')}
+          disabled={ messages.length < 1}
+        >
+          Enviar
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/pages/GeneralChat/styles.ts
+++ src/components/pages/GeneralChat/styles.ts
@@ -2,3 +2,5 @@ export const chatDiv = `h-full flex flex-col gap-4`;
 export const chatInput = `input input-bordered input-info bg-transparent
   placeholder:text-zinc-900 placeholder:dark:text-zinc-300 text-zinc-900 dark:text-zinc-300
   w-full`;
+export const userInputs = `flex gap-2`;
+export const btn = `btn btn-success`;
