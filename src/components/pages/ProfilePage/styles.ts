f9a1fafe66cca47e7b6cb7a7ae73367f8ff53c51
Rafael <1rafaelrocha2@gmail.com>
2022-11-02 10:13:34 -0300

    fix: image styles

--- src/components/pages/ProfilePage/styles.ts
+++ src/components/pages/ProfilePage/styles.ts
@@ -1,4 +1,4 @@
 export const divFather = `flex justify-center items-center m-auto gap-[10px] font-semibold max-w-[60%]`
-export const imgProfile = `rounded-full`;
+export const imgProfile = `rounded-full h-[100px] w-[100px]`;
 export const textDiv = `flex flex-col gap-2`
 export const text = `text-zinc-800 dark:text-zinc-200`;
